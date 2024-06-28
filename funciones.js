cf3fbec8dca9e0738e5572c6adf18654b578b708
daniel <danma1696@gmail.com>
2024-06-28 16:29:55 +0200

    Iniciando repositorio

---
+++ funciones.js
@@ -0,0 +1,64 @@
+function abrirVentana() {
+    let ventana = window.open("", "NuevaVentana", "width=500,height=700");
+    ventana.document.write("Esta es una nueva ventana<br>");
+    ventana.document.write("<html><head><title>Esta ventana es un pop</title></head>");
+    ventana.document.write("<body><img src='img/casa.jpg' alt='casa'></body></html>");
+    ventana.moveTo(700, 100);
+    ventana.document.body.style.backgroundColor = 'white';
+    ventana.document.body.style.color = 'white';
+}
+
+function abrirVentana1() {
+    let ventana = window.open("", "NuevaVentana", "width=500,height=700");
+     ventana.document.write("Esta es una nueva ventana<br>");
+    ventana.document.write("<html><head><title>Esta ventana es un pop</title></head>");
+    ventana.document.write("<body><img src='img/panteraRosa.jpg'></body></html>");
+    ventana.moveTo(700, 100);
+    ventana.document.body.style.backgroundColor = 'white';
+    ventana.document.body.style.color = 'white';
+}
+function abrirVentana2() {
+    let ventana = window.open("", "NuevaVentana", "width=500,height=700");
+    ventana.document.write("Esta es una nueva ventana<br>");
+    ventana.document.write("<html><head><title>Esta ventana es un pop</title></head>");
+    ventana.document.write("<body><img src='img/cuadrados.jpg'></body></html>");
+    ventana.moveTo(700, 100);
+    ventana.document.body.style.backgroundColor = 'white';
+    ventana.document.body.style.color = 'white';
+}
+function abrirVentana3(){
+    let ventana=window.open("","NuevaVentana","width=500,height=700");
+    ventana.document.write("Esta es una nueva ventana<br>");
+    ventana.document.write("<html><head><title>Esta ventana es un pop</title></head>");
+    ventana.document.write("<body><img src='img/corel3.jpg'></body></html>");
+    ventana.moveTo(700,100);
+    ventana.document.body.style.backgroundColor='white';
+    ventana.document.body.style.color='white';
+}
+function abrirVentana4(){
+    let ventana=window.open("","NuevaVentana","width=500,height=700");
+    ventana.document.write("Esta es una nueva ventana<br>");
+    ventana.document.write("<html><head><title>Esta ventana es un pop</title></head>");
+    ventana.document.write("<body><img src='img/corel2.jpg'></body></html>");
+    ventana.moveTo(700,100);
+    ventana.document.body.style.backgroundColor='white';
+    ventana.document.body.style.color='white';
+}
+function abrirVentana5(){
+    let ventana=window.open("","NuevaVentana","width=500,height=700");
+    ventana.document.write("Esta es una nueva ventana<br>");
+    ventana.document.write("<html><head><title>Esta ventana es un pop</title></head>");
+    ventana.document.write("<body><img src='img/corel4.jpg'></body></html>");
+    ventana.moveTo(700,100);
+    ventana.document.body.style.backgroundColor='white';
+    ventana.document.body.style.color='white';
+}
+function abrirVentana6(){
+    let ventana=window.open("","NuevaVentana","width=500,height=700");
+    ventana.document.write("Esta es una nueva ventana<br>");
+    ventana.document.write("<html><head><title>Esta ventana es un pop</title></head>");
+    ventana.document.write("<body><img src='img/vectorCalavera.jpg' style='width:300px;text-align:center;'></body></html>");
+    ventana.moveTo(700,100);
+    ventana.document.body.style.backgroundColor='white';
+    ventana.document.body.style.color='white';
+}
